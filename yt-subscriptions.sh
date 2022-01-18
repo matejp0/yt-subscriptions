2dc80311caa754226cdfd21a9e601c8c63a8719c
Matěj Přerovský <bofin@skaut.cz>
2022-01-18 09:43:55 +0100

    Fixed wrong code

--- yt-subscriptions.sh
+++ yt-subscriptions.sh
@@ -4,7 +4,7 @@ let num=0
 while IFS=, read -r id url name
 do
     if [[ $num -gt 0 ]]
-        then echo "$name	$id" >> "$2"
+        then echo "$id	$name" >> "$2"
         else num=$((var++))
     fi
 done < "$1"
